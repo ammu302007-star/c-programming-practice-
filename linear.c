7532ee2d7b15586db2cda2e9006098d6d03fcfe1
ammu302007-star <ammu302007@gmail.com>
2026-09-11 22:18:18 +0530

    Create linear.c

---
+++ linear.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int target;
+    int found = 0;
+
+    printf("Enter element to search: ");
+    scanf("%d", &target);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == target)
+        {
+            printf("Element found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
